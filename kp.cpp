da76ceb68049c4a8916ae872fbce1fcbccf36a50
Ankush1369 <ankush2001garg@gmail.com>
2021-04-06 12:16:07 +0530

    knapsack problem

---
+++ kp.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int knapsack_recursive(int w[], int v[], int n, int total_W)
+{
+    if(n==0 || total_W==0)
+    {
+        return 0;
+    }
+    int a = knapsack_recursive(w, v, n-1, total_W);
+    if(total_W>=w[n-1])
+    {
+        a = max(a, (v[n-1]+knapsack_recursive(w, v, n-1, total_W-w[n-1])));
+    }
+
+    return a;
+}
+
+int knapsack_dp(int w[], int v[], int n, int total_W)
+{
+    vector<vector<int> > dp(total_W+1, vector<int>(n+1, 0));
+    for(int i=1; i<=total_W; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            dp[i][j] = dp[i][j-1];
+            if(w[j-1]<=i)
+            {
+                dp[i][j] = max(dp[i][j], v[j-1]+dp[i-w[j-1]][j-1]);
+            }
+        }
+    }
+
+    return dp[total_W][n];
+
+}
+
+int main()
+{
+    int w[] = {1, 5, 3, 4};
+    int v[] = {15, 10, 9, 5};
+    int n = 4;
+    int total_W = 8;
+    cout << "Using recursive knapsack: " << knapsack_recursive(w, v, n, total_W) << '\n';
+    cout << "Using knapsack dp: " << knapsack_dp(w, v, n, total_W) << '\n';
+}
+
+
